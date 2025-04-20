0ec9fcfb7c107ffccdfa1bd5c5cb514ea4d78127
Tran Dat, Le <trandatnh@gmail.com>
2025-04-20 09:17:00 -0700

    nvim: ctrl+q to quit all
    to be the same with other editors (vscode, intellij)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map Ctrl+q to quit all
+vim.keymap.set("n", "<C-q>", "<cmd>qa<cr>", { desc = "Quit all" })
