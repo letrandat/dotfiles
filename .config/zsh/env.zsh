9639a3ea50f32e74c3897458fe219ef50afc2245
Tran Dat, Le <trandatnh@gmail.com>
2025-05-01 11:48:03 -0700

    zshrc: nvm don't need use --lts

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -12,7 +12,6 @@ export PATH="/opt/homebrew/opt/python@3.13/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm use --lts
 
 # RUBY
 export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
