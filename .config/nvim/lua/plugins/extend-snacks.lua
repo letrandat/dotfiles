713132c8146e24a7da764fed20d84ee20fb1b603
Tran Dat, Le <trandatnh@gmail.com>
2025-05-21 19:53:34 -0700

    nvim: snacks exlorer configuration

--- .config/nvim/lua/plugins/extend-snacks.lua
+++ .config/nvim/lua/plugins/extend-snacks.lua
@@ -1,10 +1,17 @@
 return {
   "snacks.nvim",
   opts = function(_, opts)
+    -- Add a custom option to the dashboard "Select Session"
     table.insert(
       opts.dashboard.preset.keys,
       7,
       { icon = "S", key = "S", desc = "Select Session", action = require("persistence").select }
     )
+
+    -- Configure explorer to show hidden/ignored files ut exclude some from search
+    opts.picker = opts.picker or {}
+    opts.picker.hidden = true
+    opts.picker.ignored = true
+    opts.picker.exclude = { "node_modules", ".git" }
   end,
 }
