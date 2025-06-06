1adc6da25a0cfde180684fd95e4c96d9a33a488c
Tran Dat, Le <trandatnh@gmail.com>
2025-06-06 13:09:53 -0700

    add hilighted yank

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -12,5 +12,18 @@ vim.schedule(function()
     vim.o.clipboard = 'unnamedplus'
 end)
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {
+        clear = true
+    }),
+    callback = function()
+        vim.hl.on_yank()
+    end
+})
+
 -- [[ Basic Keymaps ]]
 require('keymaps').setup()
