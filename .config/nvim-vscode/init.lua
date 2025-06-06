0ade876a2c23c9b2b00b7821934394e848463867
Tran Dat, Le <trandatnh@gmail.com>
2025-06-06 12:58:37 -0700

    add clipboard setting

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -3,5 +3,14 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+    vim.o.clipboard = 'unnamedplus'
+end)
+
 -- [[ Basic Keymaps ]]
 require('keymaps').setup()
