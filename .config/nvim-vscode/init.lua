89ae910b7d7a7b95801e7edd6e6800dd7467230d
Tran Dat, Le <trandatnh@gmail.com>
2025-06-06 14:40:31 -0700

    send notification from neovim to vscode

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -1,3 +1,7 @@
+local vscode = require('vscode')
+-- set vscode.notify as default notify function.
+vim.notify = vscode.notify
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
