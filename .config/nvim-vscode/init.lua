894b4fb95af3585b81a54d13e4191902b8f8707b
Tran Dat, Le <trandatnh@gmail.com>
2025-06-06 19:13:35 -0700

    moeeeeeee plugins

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -137,3 +137,19 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         error('Error cloning lazy.nvim:\n' .. out)
     end
 end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
