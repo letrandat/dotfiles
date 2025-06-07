b43bd1aad1dfba20b5caccf7fb472966ebefc0c8
Tran Dat, Le <trandatnh@gmail.com>
2025-06-06 21:45:55 -0700

    init oil

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -82,3 +82,41 @@ rtp:prepend(lazypath)
 require("lazy").setup({{
     import = "plugins"
 }})
+
+-- Default oil.code keymaps
+if vim.g.vscode then
+    local vscode = require('vscode')
+    local map = vim.keymap.set
+    vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+        pattern = {"*"},
+        callback = function()
+            map("n", "-", function()
+                vscode.action('oil-code.open')
+            end)
+        end
+    })
+
+    vim.api.nvim_create_autocmd({'FileType'}, {
+        pattern = {"oil"},
+        callback = function()
+            map("n", "-", function()
+                vscode.action('oil-code.openParent')
+            end)
+            map("n", "_", function()
+                vscode.action('oil-code.openCwd')
+            end)
+            map("n", "<CR>", function()
+                vscode.action('oil-code.select')
+            end)
+            map("n", "<C-t>", function()
+                vscode.action('oil-code.selectTab')
+            end)
+            map("n", "<C-l>", function()
+                vscode.action('oil-code.refresh')
+            end)
+            map("n", "`", function()
+                vscode.action('oil-code.cd')
+            end)
+        end
+    })
+end
