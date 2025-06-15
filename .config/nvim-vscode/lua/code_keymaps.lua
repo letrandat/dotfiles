525b56be013733a3a156eebd36df2cb3a2b311e5
Tran Dat, Le <trandatnh@gmail.com>
2025-06-15 11:17:23 -0700

    making test coverage async for go

--- .config/nvim-vscode/lua/code_keymaps.lua
+++ .config/nvim-vscode/lua/code_keymaps.lua
@@ -229,8 +229,8 @@ end, {
 keymap("n", "gc", function()
 	local lang = vim.bo.filetype
 	if lang == "go" then
-		vim.notify(string.format('Running test coverage for "%s"', lang))
-		vscode.call("go.test.coverage")
+		vim.notify(string.format('Toggle test coverage for "%s"', lang))
+		vscode_async("go.test.coverage")
 	else
 		vim.notify(string.format('Toggle test coverage is not supported for file type "%s"', lang))
 	end
