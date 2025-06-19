1777e7acd786809b50f868150ebb25ae960e5f83
Tran Dat, Le <trandatnh@gmail.com>
2025-06-19 10:01:57 -0700

    update keybinds for nvim plugins

--- .config/nvim-vscode/lua/plugins/nvim-various-textobjs.lua
+++ .config/nvim-vscode/lua/plugins/nvim-various-textobjs.lua
@@ -6,4 +6,20 @@ return {
 			useDefaults = true,
 		},
 	},
+	config = function(_, opts)
+		require("various-textobjs").setup(opts)
+
+		local keymap = vim.keymap.set
+		local keyopts = { silent = true, noremap = true }
+
+		-- ae (select entire buffer) similar to vscodevim
+		keymap({ "o", "x" }, "ae", function()
+			require("various-textobjs").entireBuffer()
+		end, keyopts)
+
+		-- O (go to next closing bracket)
+		keymap({ "o", "x" }, "O", function()
+			require("various-textobjs").toNextClosingBracket()
+		end, keyopts)
+	end,
 }
