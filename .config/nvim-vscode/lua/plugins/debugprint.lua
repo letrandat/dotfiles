10d51e8078a1c206eaf97d1d36e6e4e0c285b78a
Tran Dat, Le <trandatnh@gmail.com>
2025-06-24 19:59:15 -0700

    setup debugprint

---
+++ .config/nvim-vscode/lua/plugins/debugprint.lua
@@ -0,0 +1,39 @@
+return {
+	"andrewferrier/debugprint.nvim",
+	opts = {
+		keymaps = {
+			normal = {
+				plain_below = "gpp",
+				plain_above = "gpP",
+				variable_below = "gpv",
+				variable_above = "gpV",
+				variable_below_alwaysprompt = "",
+				variable_above_alwaysprompt = "",
+				surround_plain = "gpsp",
+				surround_variable = "gpsv",
+				surround_variable_alwaysprompt = "",
+				textobj_below = "gpo",
+				textobj_above = "gpO",
+				textobj_surround = "gpso",
+				toggle_comment_debug_prints = "",
+				delete_debug_prints = "",
+			},
+			insert = {
+				plain = "",
+				variable = "",
+			},
+			visual = {
+				variable_below = "gpv",
+				variable_above = "gpV",
+			},
+		},
+	},
+
+	dependencies = {
+		"echasnovski/mini.nvim", -- Optional: Needed for line highlighting (full mini.nvim plugin)
+		"ibhagwan/fzf-lua", -- Optional: If you want to use the `:Debugprint search` command with fzf-lua
+	},
+
+	lazy = false, -- Required to make line highlighting work before debugprint is first used
+	version = "*", -- Remove if you DON'T want to use the stable version
+}
