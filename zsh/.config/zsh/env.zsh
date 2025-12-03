8834240c4937b3cf91a370e7fb2b585f344806cf
Tran Dat, Le <trandatnh@gmail.com>
2025-12-03 11:14:21 -0800

    quick update dec 3

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -18,6 +18,14 @@ export NVM_DIR="$HOME/.nvm"
 # nvm alias default stable
 nvm use lts/krypton
 
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# bun completions
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+
+
 # RUBY
 export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 
