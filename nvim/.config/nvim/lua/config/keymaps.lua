28cd91b1b46a3271a5d439d25b936f7980d58d67
Tran Dat, Le <trandatnh@gmail.com>
2025-12-22 16:32:36 -0800

    Revert "refactor(nvim): extract shared function for file reference copying"
    This reverts commit 0097ffedc7186b60451bb30980e0b126ef6192d9.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -37,8 +37,14 @@ map("v", "<C-j>", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
 -- File Path Copying
 -- ============================================================================
 -- Copy relative path from project root
--- Helper function to copy file path with line range
-local function copy_file_reference()
+map("n", "<leader>fp", function()
+  local path = vim.fn.expand("%:.")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path, vim.log.levels.INFO)
+end, { desc = "Copy relative file path" })
+
+-- Copy relative path with line range in visual mode
+map("v", "<leader>fp", function()
   local path = vim.fn.expand("%:.")
   local start_line = vim.fn.line("v")
   local end_line = vim.fn.line(".")
@@ -48,19 +54,20 @@ local function copy_file_reference()
   local ref = "@" .. path .. "#L" .. start_line .. "-" .. end_line
   vim.fn.setreg("+", ref)
   vim.notify("Copied: " .. ref, vim.log.levels.INFO)
-end
-
--- Copy relative path with line range in visual mode
-map("v", "<leader>fp", copy_file_reference, { desc = "Copy relative file path with line range" })
+end, { desc = "Copy relative file path with line range" })
 
 -- Quick shortcut for copying file reference in visual mode
-map("v", "gy", copy_file_reference, { desc = "Copy file reference (quick)" })
-
-map("n", "<leader>fp", function()
+map("v", "gy", function()
   local path = vim.fn.expand("%:.")
-  vim.fn.setreg("+", path)
-  vim.notify("Copied: " .. path, vim.log.levels.INFO)
-end, { desc = "Copy relative file path" })
+  local start_line = vim.fn.line("v")
+  local end_line = vim.fn.line(".")
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+  local ref = "@" .. path .. "#L" .. start_line .. "-" .. end_line
+  vim.fn.setreg("+", ref)
+  vim.notify("Copied: " .. ref, vim.log.levels.INFO)
+end, { desc = "Copy file reference (quick)" })
 
 -- Copy absolute path
 map("n", "<leader>fP", function()
