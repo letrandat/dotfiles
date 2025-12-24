c62ed3b430206b12a61935c1a4c17f15095e15c6
Dat <trandatnh@gmail.com>
2025-12-24 14:24:14 -0800

    experiment with floating

---
+++ nvim/.config/nvim/lua/plugins/extend-claudecode.lua
@@ -0,0 +1,30 @@
+local toggle_key = "<M-,>" -- Alt/Meta + comma
+return {
+  {
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    keys = {
+      { toggle_key, "<cmd>ClaudeCodeFocus<cr>", desc = "Claude Code", mode = { "n", "x" } },
+    },
+    opts = {
+      terminal = {
+        snacks_win_opts = {
+          position = "float",
+          width = 0.80,
+          height = 0.80,
+          border = "rounded",
+          keys = {
+            claude_hide = {
+              toggle_key,
+              function(self)
+                self:hide()
+              end,
+              mode = "t",
+              desc = "Hide",
+            },
+          },
+        },
+      },
+    },
+  },
+}
