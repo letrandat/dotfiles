0111a36df16fb353ce13efe88f6384e6bb81d39d
Tran Dat, Le <trandatnh@gmail.com>
2025-12-23 17:20:26 -0800

    simplify the configuration for vim-jsonnet

--- nvim/.config/nvim/lua/plugins/vim-jsonnet.lua
+++ nvim/.config/nvim/lua/plugins/vim-jsonnet.lua
@@ -1,28 +1,8 @@
 return {
-  -- 1. Install the vim-jsonnet plugin
+  -- Core jsonnet plugin
   { "google/vim-jsonnet" },
 
-  -- 2. (Optional) Add Treesitter support for better syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "jsonnet" })
-      end
-    end,
-  },
-
-  -- 3. (Optional) Auto-install the LSP via Mason and set it up via lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        jsonnet_ls = {}, -- This enables the Jsonnet Language Server
-      },
-    },
-  },
-
-  -- 4. Ensure Mason installs the language server
+  -- Auto-install the LSP server (mason-lspconfig will auto-enable it)
   {
     "mason-org/mason.nvim",
     opts = function(_, opts)
