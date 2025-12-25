02f63625cced008620c65235e5daf5a6d50d9f24
Dat <trandatnh@gmail.com>
2025-12-24 16:29:16 -0800

    try to see diffs

--- nvim/.config/nvim/lua/plugins/extend-claudecode.lua
+++ nvim/.config/nvim/lua/plugins/extend-claudecode.lua
@@ -7,23 +7,31 @@ return {
       { toggle_key, "<cmd>ClaudeCodeFocus<cr>", desc = "Claude Code", mode = { "n", "x" } },
     },
     opts = {
-      terminal = {
-        snacks_win_opts = {
-          position = "float",
-          width = 0.9,
-          height = 0.9,
-          border = "rounded",
-          keys = {
-            claude_hide = {
-              toggle_key,
-              function(self)
-                self:hide()
-              end,
-              mode = "t",
-              desc = "Hide",
-            },
-          },
-        },
+      -- terminal = {
+      --   snacks_win_opts = {
+      --     position = "float",
+      --     width = 0.9,
+      --     height = 0.9,
+      --     border = "rounded",
+      --     keys = {
+      --       claude_hide = {
+      --         toggle_key,
+      --         function(self)
+      --           self:hide()
+      --         end,
+      --         mode = "t",
+      --         desc = "Hide",
+      --       },
+      --     },
+      --   },
+      -- },
+
+      -- Diff Integration
+      diff_opts = {
+        auto_close_on_accept = true,
+        vertical_split = true,
+        open_in_current_tab = true,
+        keep_terminal_focus = false, -- If true, moves focus back to terminal after diff opens
       },
     },
   },
