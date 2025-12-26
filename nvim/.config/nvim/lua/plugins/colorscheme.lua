cd2536f7fca7e01e95fde49296764508f2a7faad
Dat <trandatnh@gmail.com>
2025-12-25 21:35:10 -0800

    add cat

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,25 @@
 return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      -- choose from: storm, moon, night, and day.
+      style = "moon",
+    },
+  },
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  {
+    "catppuccin/nvim",
+    opts = {
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
 }
