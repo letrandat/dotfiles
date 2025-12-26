33e4b259c0d83740980949cacc04e388e7ecd25d
Dat <trandatnh@gmail.com>
2025-12-26 09:54:10 -0800

    leave it as default

--- nvim/.config/nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/.config/nvim/lua/plugins/auto-dark-mode.lua
@@ -1,13 +1,7 @@
 return {
   "f-person/auto-dark-mode.nvim",
   opts = {
-    set_dark_mode = function()
-      vim.api.nvim_set_option_value("background", "dark", {})
-      vim.cmd("Catppuccin frappe")
-    end,
-    set_light_mode = function()
-      vim.api.nvim_set_option_value("background", "light", {})
-      vim.cmd("Catppuccin latte")
-    end,
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
   },
 }
