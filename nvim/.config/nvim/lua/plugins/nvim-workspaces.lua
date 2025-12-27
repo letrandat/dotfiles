1005d3497159ba72616d8de91996f8eaddc1e1b8
Dat <trandatnh@gmail.com>
2025-12-27 12:10:23 -0800

    update nvim-workspace

--- nvim/.config/nvim/lua/plugins/nvim-workspaces.lua
+++ nvim/.config/nvim/lua/plugins/nvim-workspaces.lua
@@ -9,16 +9,6 @@ return {
       })
     end,
     cmd = "Workspaces",
-    keys = {
-      { "<leader>W", group = "workspaces" },
-      { "<leader>Wa", "<cmd>Workspaces add<cr>", desc = "Add Workspace Folder" },
-      { "<leader>Wr", "<cmd>Workspaces remove<cr>", desc = "Remove Workspace Folder" },
-      { "<leader>Wl", "<cmd>Workspaces load<cr>", desc = "Load Workspace" },
-      { "<leader>Ws", "<cmd>Workspaces save<cr>", desc = "Save Workspace" },
-      { "<leader>WL", "<cmd>Workspaces list<cr>", desc = "List Workspace Folders" },
-      { "<leader>Wc", "<cmd>Workspaces clear<cr>", desc = "Clear Workspace" },
-      { "<leader>Wd", "<cmd>Workspaces delete<cr>", desc = "Delete Saved Workspace" },
-      { "<leader>Wf", "<cmd>Workspaces find<cr>", desc = "Find Files in Workspace" },
-    },
+    event = "VeryLazy",
   },
 }
